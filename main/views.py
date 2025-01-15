eb02ab202b898b228f765a754a24a6344fa99457
Izaako04 <isaac.criollo04@gmail.com>
2025-01-15 14:57:13 -0500

    Terminado

--- main/views.py
+++ main/views.py
@@ -1,7 +1,37 @@
+import requests
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
+@login_required
+@permission_required('main.index_viewer', raise_exception=True)
 def index(request):
-    # return HttpResponse("Hello, World!")
-    #return render(request, 'main/base.html')
-    return render(request, 'main/index.html')
+    
+    # Arme el endpoint del REST API
+    current_url = request.build_absolute_uri()
+    url = current_url + '/api/v1/landing'
+
+    # Petición al REST API
+    response_http = requests.get(url)
+    response_dict = json.loads(response_http.content)
+
+    print("Endpoint ", url)
+    print("Response ", response_dict)
+
+    # Respuestas totales
+    total_responses = len(response_dict.keys())
+
+    # Valores de la respuesta
+    responses = response_dict.values()
+
+    # Objeto con los datos a renderizar
+    data = {
+        'title': 'Landing - Dashboard',
+        'total_responses': total_responses,
+        'responses': responses
+    }
+
+    # Renderización en la plantilla
+    return render(request, 'main/index.html', data)
